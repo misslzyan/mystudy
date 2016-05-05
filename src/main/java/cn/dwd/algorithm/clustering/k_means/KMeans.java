5e7064a29dfe27f28d6f1c8f32699849031cdf6a
weidongduan <weidongduan@sohu-inc.com>
2016-05-05 18:29:51 +0800

    k_means begin

---
+++ src/main/java/cn/dwd/algorithm/clustering/k_means/KMeans.java
@@ -0,0 +1,25 @@
+package cn.dwd.algorithm.clustering.k_means;
+
+/**
+ * 描述：kmeans算法，聚类算法。
+ * @author weidongduan
+ *
+ */
+public class KMeans {
+	
+	/**
+	 * 输入一组数据，分小、中、大三波。
+	 */
+	public int[][] kMeansInt(int[] values){
+		int a= values[0];
+		int b = values[1];
+		int c = values[2];
+		for(int i=0;i<values.length;i++){
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
