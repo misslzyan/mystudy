a6279fff043bd444bb7481f65c4b110d5101d88b
dwd <misslzyan@sina.com>
2016-10-10 22:47:25 +0800

    添加  异或 对加密的应用

---
+++ src/main/java/cn/dwd/xor/AddSecret.java
@@ -0,0 +1,27 @@
+package cn.dwd.xor;
+
+/**
+ * 利用异或对数据进行加密
+ * Created by xiaoyanzi on 16/10/10.
+ */
+public class AddSecret {
+
+    public static void main(String[] args) {
+        char c1 = '我';
+        char c2 = '是';
+        char c3 = '人';
+        char secret = '%';
+        char s1 = (char)(c1^secret);
+        char s2 = (char)(c2^secret);
+        char s3 = (char)(c3^secret);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        int n1 = s1^secret;
+        int n2 = s2^secret;
+        int n3 = s3^secret;
+        System.out.println((char)n1);
+        System.out.println((char)n2);
+        System.out.println((char)n3);
+    }
+}
